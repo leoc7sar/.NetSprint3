36b801f85510e7cd98a7496095d6c7c03a193b98
leoc7sar <161585547+leoc7sar@users.noreply.github.com>
2025-10-01 12:45:15 -0300

    Delete Mottu-API-master directory

--- Mottu-API-master/Contracts/UsuariosDtos.cs
+++
@@ -1,9 +0,0 @@
-namespace Mottu.Api.Contracts;
-
-public record UsuarioResponse(Guid Id, string Nome, string Email);
-public record UsuarioCreateRequest(string Nome, string Email, string Senha);
-public record UsuarioUpdateRequest(string Nome, string? Senha); 
-public record UsuarioDeleteRequest(string Email);
-
-public record LoginRequest(string Email, string Senha);
-public record AuthResponse(string Token, UsuarioResponse Usuario);
